ddb47153d7a9f2d6b2735f81034b810ef002dfb5
GlendyT <glendytuyuc@gmail.com>
2025-11-20 22:49:19 -0600

    adding improvements to the widget

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -6,8 +6,9 @@ import { WidgetErrorScreen } from "../screens/widget-error-screen";
 import { WidgetLoadingScreen } from "../screens/widget-loading-screen";
 import { WidgetSelectionScreen } from "../screens/widget-selection-screen";
 import { WidgetChatScreen } from "../screens/widget-chat-screen";
-import { WidgetInboxScreen } from '../screens/widget-inbox-screen';
-import { WidgetVoiceScreen } from '../screens/widget-voice-screen';
+import { WidgetInboxScreen } from "../screens/widget-inbox-screen";
+import { WidgetVoiceScreen } from "../screens/widget-voice-screen";
+import { WidgetContactScreen } from "../screens/widget-contact-screen";
 
 interface Props {
   organizationId: string | null;
@@ -20,15 +21,15 @@ export const WidgetView = ({ organizationId }: Props) => {
     error: <WidgetErrorScreen />,
     loading: <WidgetLoadingScreen organizationId={organizationId} />,
     auth: <WidgetAuthScreen />,
-    voice: <WidgetVoiceScreen/>,
-    inbox: <WidgetInboxScreen/>,
+    voice: <WidgetVoiceScreen />,
+    inbox: <WidgetInboxScreen />,
     selection: <WidgetSelectionScreen />,
     chat: <WidgetChatScreen />,
-    contact: <div>Contact</div>,
+    contact: <WidgetContactScreen />,
   };
 
   return (
-    <main className=" min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
+    <main className=" flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
       {screenCompoents[screen]}
     </main>
   );
